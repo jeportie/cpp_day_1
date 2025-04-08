0c816ec3c6326d9d618d47adc5df6aa08195cc9d
jeportie <jeportie@student.42.fr>
2025-04-08 13:38:46 +0000

    push pause

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,8 +6,21 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 11:33:30 by jeportie          #+#    #+#             */
-/*   Updated: 2025/04/08 12:05:41 by jeportie         ###   ########.fr       */
+/*   Updated: 2025/04/08 13:26:02 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "src/class/Harl.hpp"
+#include <string>
+#include <iostream>
 
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <log level>" << std::endl;
+        return 1;
+    }
+
+    Harl harl;
+    harl.complain(argv[1]);
+    return 0;
+}
